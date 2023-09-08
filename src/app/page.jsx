c806fd08f10d4b7d43e29e5632ad5afa9651c5da
Rahul Sharma <rahulsharma02121998@gmail.com>
2023-09-08 17:06:33 +0530

    css responsive layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,29 @@
-import styles from "./homepage.module.css";
+import styles from './homepage.module.css';
 
 export default function Home() {
-  return <div>Hello</div>;
+    return (
+        <div>
+            <h1>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Excepturi, culpa sed. Eius nobis officiis est impedit tempore
+                obcaecati rem dolorem pariatur soluta hic, sequi, nulla fuga et
+                illo quo ad sed vitae?
+            </h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+                repellat ipsam nulla consectetur vitae quibusdam deleniti sint
+                possimus error et minus cupiditate libero a nobis cum
+                repellendus odit nostrum aliquid alias illum adipisci aperiam,
+                perferendis ducimus suscipit. Consequuntur, vitae doloribus
+                illum temporibus, quaerat commodi doloremque quisquam earum,
+                placeat totam quam aliquam? Esse quibusdam ad, rerum nesciunt
+                nam aperiam eum modi aliquid repellat ut nostrum quo nihil quam
+                cupiditate? Ullam a explicabo quis blanditiis repudiandae
+                incidunt esse commodi voluptate. Praesentium quia explicabo
+                delectus unde aperiam recusandae, soluta earum laborum debitis
+                mollitia! Numquam obcaecati in fugit unde repudiandae accusamus
+                provident voluptatem odio?
+            </p>
+        </div>
+    );
 }
