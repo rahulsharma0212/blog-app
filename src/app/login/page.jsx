5c68d7f9f6c2a0f869747eb54fa117615e03bf1b
Rahul Sharma <rahulsharma02121998@gmail.com>
2023-09-10 15:05:39 +0530

    mongodb login setup using prisma as a adapter

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -2,33 +2,43 @@
 import { signIn, useSession } from 'next-auth/react';
 import styles from './loginPage.module.css';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 const LoginPage = () => {
     const { status } = useSession();
 
     const router = useRouter();
 
-    if (status === 'loading') {
-        return <div className={styles.loading}>Loading...</div>;
-    }
-
-    if (status === 'authenticated') {
-        router.push('/');
-    }
+    useEffect(() => {
+        if (status === 'authenticated') {
+            router.push('/');
+        }
+    }, [status]);
 
     return (
-        <div className={styles.container}>
-            <div className={styles.wrapper}>
-                <div
-                    className={styles.socialButton}
-                    onClick={() => signIn('google')}
-                >
-                    Sign in with Google
+        <>
+            {status === 'loading' && (
+                <div className={styles.loading}>Loading...</div>
+            )}
+            {status === 'unauthenticated' && (
+                <div className={styles.container}>
+                    <div className={styles.wrapper}>
+                        <div
+                            className={styles.socialButton}
+                            onClick={() => signIn('google')}
+                        >
+                            Sign in with Google
+                        </div>
+                        <div className={styles.socialButton}>
+                            Sign in with Github
+                        </div>
+                        <div className={styles.socialButton}>
+                            Sign in with Facebook
+                        </div>
+                    </div>
                 </div>
-                <div className={styles.socialButton}>Sign in with Github</div>
-                <div className={styles.socialButton}>Sign in with Facebook</div>
-            </div>
-        </div>
+            )}
+        </>
     );
 };
 
--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -2,61 +2,90 @@
 
 import Image from 'next/image';
 import styles from './writePage.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.bubble.css';
+import { useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 
 const WritePage = () => {
+    const router = useRouter();
+
     const [isOpen, setIsOpen] = useState(false);
     const [value, setValue] = useState('');
+
+    const { status } = useSession();
+
+    useEffect(() => {
+        if (status === 'unauthenticated') {
+            router.push('/');
+        }
+    }, [status]);
+
     return (
-        <div className={styles.container}>
-            <input type="text" placeholder="Title" className={styles.input} />
-            <div className={styles.editor}>
-                <button
-                    className={styles.button}
-                    onClick={() => setIsOpen((e) => !e)}
-                >
-                    <Image src="/plus.png" alt="" width={16} height={16} />
-                </button>
-                {isOpen && (
-                    <div className={styles.add}>
-                        <div className={styles.addButton}>
+        <>
+            {status === 'loading' && (
+                <div className={styles.loading}>Loading...</div>
+            )}
+            {status === 'authenticated' && (
+                <div className={styles.container}>
+                    <input
+                        type="text"
+                        placeholder="Title"
+                        className={styles.input}
+                    />
+                    <div className={styles.editor}>
+                        <button
+                            className={styles.button}
+                            onClick={() => setIsOpen((e) => !e)}
+                        >
                             <Image
-                                src="/image.png"
+                                src="/plus.png"
                                 alt=""
                                 width={16}
                                 height={16}
                             />
-                        </div>
-                        <div className={styles.addButton}>
-                            <Image
-                                src="/external.png"
-                                alt=""
-                                width={16}
-                                height={16}
-                            />
-                        </div>
-                        <div className={styles.addButton}>
-                            <Image
-                                src="/video.png"
-                                alt=""
-                                width={16}
-                                height={16}
-                            />
-                        </div>
+                        </button>
+                        {isOpen && (
+                            <div className={styles.add}>
+                                <div className={styles.addButton}>
+                                    <Image
+                                        src="/image.png"
+                                        alt=""
+                                        width={16}
+                                        height={16}
+                                    />
+                                </div>
+                                <div className={styles.addButton}>
+                                    <Image
+                                        src="/external.png"
+                                        alt=""
+                                        width={16}
+                                        height={16}
+                                    />
+                                </div>
+                                <div className={styles.addButton}>
+                                    <Image
+                                        src="/video.png"
+                                        alt=""
+                                        width={16}
+                                        height={16}
+                                    />
+                                </div>
+                            </div>
+                        )}
+                        <ReactQuill
+                            className={styles.textArea}
+                            theme="bubble"
+                            value={value}
+                            onChange={setValue}
+                            placeholder="Tell your story..."
+                        />
                     </div>
-                )}
-                <ReactQuill
-                    className={styles.textArea}
-                    theme="bubble"
-                    value={value}
-                    onChange={setValue}
-                    placeholder="Tell your story..."
-                />
-            </div>
-            <button className={styles.publish}>Publish</button>
-        </div>
+                    <button className={styles.publish}>Publish</button>
+                </div>
+            )}
+        </>
     );
 };
 
